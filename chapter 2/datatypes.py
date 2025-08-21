0a08b7607784a5f6206d607fec8d3fea2885aa03
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2025-08-21 23:01:23 +0530

    Add files via upload

---
+++ chapter 2/datatypes.py
@@ -0,0 +1,12 @@
+a=1
+
+b=5.22
+
+c="mustafa"
+
+d=False
+
+e=None
+
+_mustafa=85
+
---
+++ chapter 2/operstors.py
@@ -0,0 +1,25 @@
+#Arithmetic Operators
+a = 34
+b = 4
+c = a + b
+print(c)
+
+#Assignment Operators
+a = 4-2
+print(a)
+b = 6
+b += 3
+print(b)
+
+#Comparison Operators
+d = 5!=7
+print(d)
+
+#Logical Operators
+#OR
+print("False or True is",False or True)
+print("False or False is", False or False)
+
+#And
+print("True and False is",True and False)
+print("False and True is",False and True)
