8e69b586cbd4d8185b7d31c763ef0e905cfb321a
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2025-12-06 12:04:44 +0530

    Update 02_problem.py

--- chapter 9ps/02_problem.py
+++ chapter 9ps/02_problem.py
@@ -1,22 +1,22 @@
 import random
 
 def game():
-    print("You are playing the game..")
+    print("You are playing a game.. ")
     score = random.randint(1,62)
-    # Fetch the hiscore
-    with open("hiscore.txt") as f:
+
+    with open("hiscore.txt")as f:
         hiscore = f.read()
-        if(hiscore==""):
-            hiscore = int(hiscore)
+        if(hiscore!=""):
+            hiscore = int (hiscore)
         else:
-            hiscore =0   
+            hiscore = 0
 
-    print(f"Your score:{score}")
-    if(score>hiscore):
-        #Write this hiscore to the file
-        with open("hiscore.txt","w") as f:
-            f.write(str(hiscore))
+    print(f"Your score :{score}")
+    if(score>hiscore or hiscore):
 
-    return score
+        with open("hiscore.txt","w")as f:
+            f.write(str(score))
+
+    return score 
 
 game()
